bb6f1db260c523ec4b55d67d81b21e0d09999bb1
David <david-bitner@hotmail.com>
2020-10-28 18:12:08 -0300

    Update main.py
    Added feature to select directory to save profile

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import os
+from tkinter.filedialog import askdirectory
+import tkinter as tk
 
+directory = askdirectory(title="Escolha onde salvar")
+root = tk.Tk()
+root.withdraw()
 while True:
     profile = input("Profile name: [0 to end] @")
-    os.system(f"instaloader --no-videos --no-captions --no-metadata-json --no-compress-json profile {profile}")
+    if profile == '0':
+        break
+    dirname = f'--dirname-pattern={directory}/@{profile}'
+    os.system(f"instaloader {dirname} --fast-update --no-videos --no-captions --no-metadata-json --no-compress-json {profile}")
