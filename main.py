4239d76b80301258dcc352bfe340270932a8f2b0
David <david-bitner@hotmail.com>
2020-10-29 17:53:09 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import os
 from tkinter.filedialog import askdirectory
 import tkinter as tk
 
-directory = askdirectory(title="Escolha onde salvar")
+options = ['1 - Posts', '2 - IGTV']
+options2 = ['1 - Search by Hashtag', '2 - Download Comments', '3 - Download by date', '4 - View Count', '5 - Comments Count']
+for o in options:
+    print(o)
+option = int(input('Option: '))
 root = tk.Tk()
-root.withdraw()
+directory = askdirectory(title="Save location")
+root.destroy()
 while True:
     profile = input("Profile name: [0 to end] @")
     if profile == '0':
         break
     dirname = f'--dirname-pattern={directory}/@{profile}'
-    os.system(f"instaloader {dirname} --fast-update --no-videos --no-captions --no-metadata-json --no-compress-json {profile}")
+    if option == 1:
+        os.system(f"instaloader {dirname} --no-videos --no-captions --no-metadata-json --no-compress-json {profile}")
+    elif option == 2:
+        os.system(f"instaloader {dirname} --igtv --no-posts --no-captions --no-metadata-json --no-compress-json {profile}")
