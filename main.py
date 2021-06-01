3a0ee3bdd60f1b2c66a167dcab24175678f6d9f1
David <david-bitner@hotmail.com>
2021-06-01 03:20:26 -0300

    Update main.py
    Added fast update feature

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import os
 from tkinter.filedialog import askdirectory
 import tkinter as tk
 
-options = ['1 - Posts', '2 - IGTV']
-options2 = ['1 - Search by Hashtag', '2 - Download Comments', '3 - Download by date', '4 - View Count', '5 - Comments Count']
+options = ['1 - Posts', '2 - IGTV', '3 - Fast Update']
 for o in options:
     print(o)
 option = int(input('Option: '))
@@ -19,3 +18,5 @@ while True:
         os.system(f"instaloader {dirname} --no-videos --no-captions --no-metadata-json --no-compress-json {profile}")
     elif option == 2:
         os.system(f"instaloader {dirname} --igtv --no-posts --no-captions --no-metadata-json --no-compress-json {profile}")
+    elif option == 3:
+        os.system(f"instaloader {dirname} --fast-update --no-videos --no-captions --no-metadata-json --no-compress-json {profile} ")
